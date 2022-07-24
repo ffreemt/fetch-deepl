8410fb5645a34cf6420e32b3382d5156f3eab180
freemt <yucongo+fmt@gmail.com>
2022-07-24 10:41:59 +0800

    Update 0.1.0a0 ready

--- fetch_deepl/fetch_deepl.py
+++ fetch_deepl/fetch_deepl.py
@@ -1,7 +1,55 @@
-"""Define fetch_deepl."""
+"""Fetch translation text from a deepl-fastapi API."""
+from typing import Optional
+
+import httpx
 from logzero import logger
 
+url_ = "https://hf.space/embed/mikeee/gradio-deepl/+/api/predict"
+timeout_ = httpx.Timeout(10)
 
-def fetch_deepl():
-    """Define fetch_deepl."""
-    logger.debug(" entry ")
+
+def fetch_deepl(
+    text: str,
+    from_lang: str = "en",
+    to_lang: str = "zh",
+    url: Optional[str] = None,
+    timeout: Optional[httpx.Timeout] = None,
+) -> str:
+    """Fetch translation text from a deepl-fastapi API.
+
+    >>> '测试' in fetch_deepl("test this")
+    True
+    >>> 'Test' in fetch_deepl("测试一下", 'en', 'zh')
+    True
+    """
+    if url is None:
+        url = url_
+    try:
+        url = url.strip()
+    except Exception as exc:
+        logger.error(exc)
+        url = url_
+
+    if timeout is None:
+        timeout = timeout_
+
+    data = {"data": [text, from_lang, to_lang]}
+    try:
+        resp = httpx.post(url, json=data, timeout=timeout)
+        resp.raise_for_status()
+    except Exception as exc:
+        logger.error(exc)
+        raise
+    try:
+        jdata = resp.json()
+    except Exception as exc:
+        logger.error(exc)
+        raise
+
+    try:
+        res = jdata.get("data")[0]
+    except Exception as exc:
+        logger.error(exc)
+        raise
+
+    return res
